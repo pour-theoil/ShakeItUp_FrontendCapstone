acd341321aa51f9c9fedfd2d61de33b9df3aa32a
Logan Demmy <frank.demmy@gmail.com>
2021-04-23 15:07:44 -0500

    ingredients displaying on cards

--- src/components/menus/CocktailCard.js
+++ src/components/menus/CocktailCard.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllIngredients } from '../../modules/BuilderManager'
 
 export const CocktailCard = ({ cocktail, removeCocktailFromMenu }) => {
+    const [ingredients, setIngredients] = useState([])
+    const getIngredients = () =>{
+        console.log(cocktail.id)
+        getAllIngredients(cocktail.id)
+        .then(ingredients => {
+            let ingredientsObj = ingredients.map(ingredient => ingredient.ingredient.name)
+            setIngredients(ingredientsObj)
+        }
+        )
+    }
+    console.log(ingredients)
+    useEffect(()=>{
+        getIngredients()
+    },[])
     return(
         <>
                 <article className="cocktail">
                     <h2 className="cocktail-name">{cocktail.cocktail.name}</h2>
-                    <p className="cocktail-ingredients"></p>
+                    <p className="cocktail-ingredients">{ingredients.join(", ")}</p>
                 </article>
                 <button type="button" className="article-btn"onClick={() => removeCocktailFromMenu(cocktail.id)}>Delete</button>
         </>
